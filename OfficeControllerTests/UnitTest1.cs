2133b05deaf99f1eef6886742149117202310b9d
yousef elseisi <youseffofo1995@gmail.com>
2026-06-15 00:51:23 +0100

    Add Smart Office C# project

---
+++ OfficeControllerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OfficeControllerTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
